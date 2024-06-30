a50e3ba9521da8ee735ba1b5d1ddd21bfcac45b6
Arafat <mdarafat1819@gmail.com>
2024-06-30 20:14:42 +0600

    Top Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import { CiShoppingCart, CiSearch, CiLogin } from "react-icons/ci";
+import { FaCartPlus, FaSignInAlt, FaSearch} from "react-icons/fa";
+import './Navbar.css';
+function NavBar() {
+
+    return <>
+        <nav>
+        <a href="#" className="site-name">Horticulture BD</a>
+            <div className="search-container">
+            
+            <input type="search" id="search_input" placeholder="Search..."></input>
+            <button ><FaSearch /></button>
+            </div>
+            <button type="button" id="cart_btn"><FaCartPlus className="icon" /> Cart</button>
+            <button type="button" id="login_btn"><FaSignInAlt className="icon"/> Log In</button>
+        </nav>
+    </>
+
+}
+export default NavBar;
