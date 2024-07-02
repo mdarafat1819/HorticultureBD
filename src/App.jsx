e11108cd2f954036aac7730f846160c6aacff8d1
Arafat <mdarafat1819@gmail.com>
2024-07-02 15:55:56 +0600

    slider added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import NavBar from './components/Navbar';
-import Footer from './components/footer'
+import Footer from './components/footer';
+import Slider from './components/Slider';
 function App() {
   return (
     <>
     <NavBar />
+    <Slider />
     <Footer />
     </>
   )
---
+++ src/components/Slider.jsx
@@ -0,0 +1,74 @@
+import { useState, useEffect } from "react";
+
+import './Slider.css';
+
+function Slider() {
+    const images = [
+        "https://www.w3schools.com/howto/img_nature_wide.jpg",
+        "https://www.w3schools.com/howto/img_snow_wide.jpg",
+        "https://www.w3schools.com/howto/img_mountains_wide.jpg"
+    ];
+    const [slideIndex, slideIndexUpdate] = useState(1);
+    useEffect(() => {
+        showSlides(slideIndex);
+
+        // const interval = setInterval(() => {
+        //     slideIndexUpdate(prevIndex => prevIndex + 1);
+        // }, 3000); // Change slide every 3 seconds
+
+        // return () => clearInterval(interval); // Clean
+    }, [slideIndex]);
+    function showSlides(n) {
+
+        //slideIndexUpdate(n);
+        let slides = document.getElementsByClassName("slides");
+        let dots = document.getElementsByClassName("dot");
+        if (slideIndex > slides.length) {
+            slideIndexUpdate(1)
+            console.log("Show Slide Called " + slideIndex);
+            return;
+        }
+        else if (slideIndex < 1) {
+            slideIndexUpdate(slides.length);
+            return;
+        }
+        console.log("Show Slide Called " + slideIndex);
+
+        let i;
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active", "");
+        }
+        slides[slideIndex - 1].style.display = "block";
+        dots[slideIndex - 1].className += " active";
+    }
+    return (
+        <>
+            <div className="slideshow-container">
+                {images.map((image, index) => {
+                    return <div className="slides fade">
+                        <img src={image} />
+                    </div>
+                })}
+
+                <a className="prev" onClick={() => slideIndexUpdate(slideIndex - 1)}>❮</a>
+                <a className="next" onClick={() => slideIndexUpdate(slideIndex + 1)}>❯</a>
+
+                <div className="dot-container">
+                    {
+                        images.map(
+                            (_, index) => {
+                                return <span className="dot" onClick={() => {
+                                    console.log(index);
+                                    slideIndexUpdate(index + 1);
+                                }}></span>
+                            })
+                    }
+                </div>
+            </div>
+        </>
+    );
+}
+export default Slider;
